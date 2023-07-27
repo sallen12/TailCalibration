2d8694ceacc2422c723a087c870210b256b5de4a
sallen12 <sam.allen@stat.unibe.ch>
2023-07-27 19:33:38 +0200

    add reliability diagrams to visualise probabilistic tail calibration

--- sim_study.R
+++ sim_study.R
@@ -1,3 +1,6 @@
+################################################################################
+### set up
+
 library(evmix)
 library(ggplot2)
 #devtools::install_github("sallen12/WeightedForecastVerification")
@@ -5,6 +8,8 @@ library(WeightedForecastVerification)
 
 set.seed(89)
 
+
+################################################################################
 ### simulate data
 
 gamma <- 1/4
@@ -39,6 +44,7 @@ plot_calibration <- function(df, type = "", yref = 1, title = "", ylims = NULL) 
 }
 
 
+################################################################################
 ### marginal tail calibration
 
 mtc_clim <- sapply(t_vec, function(t) (y > t)/(1 - pgpd(t, 0, 1, gamma)))
@@ -53,7 +59,7 @@ df <- data.frame(t = t_vec,
 plot_calibration(df, type = "Marginal tail calibration")
 
 
-# condition on delta
+## condition on delta
 
 del_n <- 5
 del_quants <- seq(0, 1, length.out = del_n + 1)
@@ -62,19 +68,28 @@ cal_clim <- matrix(NA, nrow = del_n, ncol = length(t_vec))
 del_plots <- list()
 for (del_i in 1:del_n) {
   print(del_i)
+  
+  # get category limits
   del_t <- quantile(delta, del_quants[c(del_i, del_i + 1)])
   if (del_i == 1) del_t[1] <- 0
   del_ind <- delta > del_t[1] & delta <= del_t[2]
 
+  # plot calibration 
   df <- data.frame(t = t_vec, 
                  q = sapply(t_vec, function(t) mean(y[del_ind] <= t)),
-                 mtc = c(colMeans((mtc_clim - 1)*del_ind), colMeans((mtc_id - 1)*del_ind), colMeans((mtc_ex - 1)*del_ind)), 
+                 mtc = c(colMeans((mtc_clim - 1)*del_ind), 
+                         colMeans((mtc_id - 1)*del_ind), 
+                         colMeans((mtc_ex - 1)*del_ind)), 
                  mth = rep(c("Clim", "Ideal", "Extremist"), each = length(t_vec)))
-  cal_clim[del_i, ] <- colSums((mtc_clim - 1)*del_ind) # check that average over all delta is the overall marginal tail calibration
   
-  del_plots[[del_i]] <-
-    plot_calibration(df, type = "Marginal tail calibration", 0, 
-                     title = paste0("Min: ", round(del_t[1], 2), ", Max: ", round(del_t[2], 2)), ylims = c(-0.5, 2.1))
+  del_plots[[del_i]] <- plot_calibration(df, 
+                                         type = "Marginal tail calibration", 
+                                         yref = 0, 
+                                         title = paste0("Min: ", round(del_t[1], 2), ", Max: ", round(del_t[2], 2)), 
+                                         ylims = c(-0.5, 2.1))
+  
+  # check that average over all delta is the overall marginal tail calibration
+  cal_clim[del_i, ] <- colSums((mtc_clim - 1)*del_ind)
 }
 
 do.call(gridExtra::grid.arrange, c(del_plots, nrow = 1))
@@ -84,7 +99,7 @@ plot(t_vec, colSums(cal_clim)/N)
 lines(t_vec, colMeans(mtc_clim - 1), col = "blue")
 
 
-
+################################################################################
 ### climatological marginal tail calibration
 
 cmtc_clim <- sapply(t_vec, function(t) mean(y > t)/(1 - pgpd(t, 0, 1, gamma)))
@@ -99,7 +114,7 @@ df <- data.frame(t = t_vec,
 plot_calibration(df, type = "Climatological marginal tail calibration")
 
 
-
+################################################################################
 ### probabilistic tail calibration
 
 ptc_clim <- sapply(t_vec, function(t) {
@@ -127,7 +142,8 @@ ptc_ex <- sapply(t_vec, function(t) {
   }
 })
 
-# mean
+
+## mean
 ptc_clim_mn <- sapply(ptc_clim, mean)
 ptc_id_mn <- sapply(ptc_id, mean)
 ptc_ex_mn <- sapply(ptc_ex, mean)
@@ -139,7 +155,8 @@ df <- data.frame(t = t_vec,
 
 plot_calibration(df, type = "Probabilistic tail calibration", 0.5)
 
-# variance
+
+## variance
 ptc_clim_var <- sapply(ptc_clim, var)
 ptc_id_var <- sapply(ptc_id, var)
 ptc_ex_var <- sapply(ptc_ex, var)
@@ -149,16 +166,18 @@ df <- data.frame(t = t_vec,
                  mtc = c(ptc_clim_var, ptc_id_var, ptc_ex_var), 
                  mth = rep(c("Clim", "Ideal", "Extremist"), each = length(t_vec)))
 
-plot_calibration(df, type = "Probabilistic tail calibration", 1/12)
+plot_calibration(df, type = "Probabilistic tail calibration", yref = 1/12)
 
-# histogram
+
+## histogram
 t <- round(quantile(y, 0.99), 1)
 pitplot_clim <- pit_hist(ptc_clim[[which(t_vec == t)]], ranks = F, ymax = 0.25, title = "Climatological")
 pitplot_id <- pit_hist(ptc_id[[which(t_vec == t)]], ranks = F, ymax = 0.25, title = "Ideal")
 pitplot_ex <- pit_hist(ptc_ex[[which(t_vec == t)]], ranks = F, ymax = 0.25, title = "Extremist")
 gridExtra::grid.arrange(pitplot_clim, pitplot_id, pitplot_ex)
 
-# divergence
+
+## divergence
 Rcpp::sourceCpp("crps_div.cpp")
 
 ptc_clim_div <- sapply(ptc_clim, crps_div)
@@ -170,10 +189,10 @@ df <- data.frame(t = t_vec,
                  mtc = c(ptc_clim_div, ptc_id_div, ptc_ex_div), 
                  mth = rep(c("Clim", "Ideal", "Extremist"), each = length(t_vec)))
 
-plot_calibration(df, type = "Probabilistic tail calibration", 0)
+plot_calibration(df, type = "Probabilistic tail calibration", yref = 0)
 
-# condition on delta
 
+## condition on delta
 ptc_clim_del <- lapply(seq_along(t_vec), function(i) cbind(ptc_clim[[i]], delta[y > t_vec[i]]))
 ptc_id_del <- lapply(seq_along(t_vec), function(i) cbind(ptc_id[[i]], delta[y > t_vec[i]]))
 ptc_ex_del <- lapply(seq_along(t_vec), function(i) cbind(ptc_ex[[i]], delta[y > t_vec[i]]))
@@ -183,8 +202,12 @@ del_quants <- seq(0, 1, length.out = del_n + 1)
 
 del_plots <- list()
 for (del_i in 1:del_n) {
+  
+  # get category limits
   del_t <- quantile(delta, del_quants[c(del_i, del_i + 1)])
   if (del_i == 1) del_t[1] <- 0
+  
+  # plot calibration 
   df <- data.frame(t = t_vec,
                    q = sapply(t_vec, function(t) mean(y[delta > del_t[1] & delta <= del_t[2]] <= t)),
                    mtc = c(sapply(ptc_clim_del, function(x) 
@@ -196,12 +219,30 @@ for (del_i in 1:del_n) {
                    mth = rep(c("Clim", "Ideal", "Extremist"), each = length(t_vec)))
   
   del_plots[[del_i]] <-
-    plot_calibration(df, type = "Probabilistic tail calibration", 0, 
+    plot_calibration(df, type = "Probabilistic tail calibration", yref = 0, 
                      title = paste0("Min: ", round(del_t[1], 2), ", Max: ", round(del_t[2], 2)))
 }
 
 do.call(gridExtra::grid.arrange, c(del_plots, nrow = 1))
 
+
+## reliability diagrams
+rd_vec <- seq(0, 20, 5)
+
+# clim
+z <- ptc_clim[sapply(rd_vec, function(t) which(t_vec == t))]
+names(z) <- sprintf("%02d", rd_vec)
+pit_reldiag(z)
+
+# id
+z <- ptc_id[sapply(rd_vec, function(t) which(t_vec == t))]
+names(z) <- sprintf("%02d", rd_vec)
+pit_reldiag(z)
+
+# ext
+z <- ptc_ex[sapply(rd_vec, function(t) which(t_vec == t))]
+names(z) <- sprintf("%02d", rd_vec)
+pit_reldiag(z)
 
 
 ### climatological probabilistic tail calibration (EXPENSIVE)
